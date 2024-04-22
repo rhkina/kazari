83937c1849d38f60e5f684343f1cbe0058d098c3
rhkina <rhkina@gmail.com>
2024-04-22 14:24:25 -0300

    First commit

---
+++ src/app/main/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+const MainPage = (props: Props) => {
+  return (
+    <div>
+      <div>Main Page</div>
+      <Button variant='destructive'>Button</Button>
+    </div>
+  )
+}
+
+export default MainPage
